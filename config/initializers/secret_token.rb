e0ea92fd4ae948150ecf48fc8a5a6d722648581c
Kerry Perez <k4torce@gmail.com>
2014-01-05 23:25:47 -0500

    first and last commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackersFestTest::Application.config.secret_key_base = 'b3a108c6c8219774ed4eeb06cf96ee9cebd1bbdc858868e7ce535274e9fcc505916dff214c4bdfa51e33c769e350f62e38317c1a548d556a686ad6cfe3249c37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackersFestTest::Application.config.session_store :cookie_store, key: '_HackersFestTest_session'
